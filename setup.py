d2332792b02da9a5c53e4cf139a72b3cea8fb6ee
MikeloVV <mikhail.voronov@gmail.com>
2026-02-11 11:25:17 +0300

    Recreate repo

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='phonebox_plugin',
+    name='netbox-phonebox',
     version='1.1.0',
     description='Phone number management for NetBox',
     url='https://github.com/MikeloVV/netbox-phonebox',
-    author='Your Name',
+    author='Mikhail Voronov',
     license='Apache 2.0',
     install_requires=[
         'phonenumbers>=8.13.0',
