23e3c31246893838bf89379e882714a85a1aac3a
MikeloVV <mikhail.voronov@gmail.com>
2026-02-11 11:43:52 +0300

    Minor changes

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-phonebox',
-    version='1.1.2',
-    description='Phone number management for NetBox',
+    version='1.1.0',
+    description='Phone number management for NetBox with E.164 validation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/MikeloVV/netbox-phonebox',
     author='Mikhail Voronov',
+    author_email='mikhail.voronov@gmail.com',
     license='Apache 2.0',
     install_requires=[
         'phonenumbers>=8.13.0',
@@ -13,4 +16,14 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
 )
