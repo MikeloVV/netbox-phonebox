d308cc1b540971bf4720fee7f04e7aab7e0d8a09
MikeloVV <mikhail.voronov@gmail.com>
2026-02-13 10:15:47 +0300

    Some fixes

--- netbox_phonebox/__init__.py
+++ netbox_phonebox/__init__.py
@@ -3,7 +3,7 @@ from netbox.plugins import PluginConfig
 class PhoneBoxConfig(PluginConfig):
     name = 'netbox_phonebox'
     verbose_name = 'PhoneBox'
-    description = 'Phone number management for NetBox with validation and normalization'
+    description = 'Phone number and PBX management for NetBox'
     version = '1.2.1'
     author = 'Mikhail Voronov'
     author_email = 'mikhail.voronov@gmail.com'
@@ -12,7 +12,7 @@ class PhoneBoxConfig(PluginConfig):
     max_version = '4.9.99'
     required_settings = []
     default_settings = {
-        'default_country_code': 'RU',
+        'default_country_code': '+7',
         'enable_qr_codes': True,
     }
 
--- netbox_phonebox/navigation.py
+++ netbox_phonebox/navigation.py
@@ -1,112 +1,106 @@
-from netbox.plugins import PluginMenuItem, PluginMenuButton
+from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
+menu = PluginMenu(
+    label='PhoneBox',
+    groups=(
+        (
+            'Phone Numbers',
+            (
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:phonenumber_list',
+                    link_text='Phone Numbers',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:phonenumber_add',
+                            title='Add',
+                            icon_class='mdi mdi-plus-thick',
+                        ),
+                    ),
+                ),
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:telephonyprovider_list',
+                    link_text='Providers',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:telephonyprovider_add',
+                            title='Add',
+                            icon_class='mdi mdi-plus-thick',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        (
+            'PBX',
+            (
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:pbxserver_list',
+                    link_text='PBX Servers',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:pbxserver_add',
+                            title='Add',
+                            icon_class='mdi mdi-plus-thick',
+                        ),
+                    ),
+                ),
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:extension_list',
+                    link_text='Extensions',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:extension_add',
+                            title='Add',
+                            icon_class='mdi mdi-plus-thick',
+                        ),
+                    ),
+                ),
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:siptrunk_list',
+                    link_text='SIP Trunks',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:siptrunk_add',
+                            title='Add',
+                            icon_class='mdi mdi-plus-thick',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        (
+            'Call Management',
+            (
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:calllog_list',
+                    link_text='Call Logs',
+                ),
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:call_statistics',
+                    link_text='Call Statistics',
+                ),
+                PluginMenuItem(
+                    link='plugins:netbox_phonebox:make_call',
+                    link_text='Make Call',
+                    buttons=(
+                        PluginMenuButton(
+                            link='plugins:netbox_phonebox:make_call',
+                            title='Make Call',
+                            icon_class='mdi mdi-phone',
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    icon_class='mdi mdi-phone-classic',
+)
+
+# Dashboard link (appears in top menu)
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_phonebox:phonenumber_dashboard',
-        link_text='Dashboard',
+        link='plugins:netbox_phonebox:dashboard',  # ← Исправлено: было phonenumber_dashboard
+        link_text='PhoneBox Dashboard',
         permissions=['netbox_phonebox.view_phonenumber'],
-        buttons=()
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:phonenumber_list',
-        link_text='Phone Numbers',
-        permissions=['netbox_phonebox.view_phonenumber'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:phonenumber_add',
-                title='Add',
-                icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_phonebox.add_phonenumber']
-            ),
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:phonenumber_import',
-                title='Import CSV',
-                icon_class='mdi mdi-upload',
-                permissions=['netbox_phonebox.add_phonenumber']
-            ),
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:phonenumber_bulk_import',
-                title='Bulk Import',
-                icon_class='mdi mdi-text-box-multiple',
-                permissions=['netbox_phonebox.add_phonenumber']
-            ),
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:phonenumber_export',
-                title='Export',
-                icon_class='mdi mdi-download',
-                permissions=['netbox_phonebox.view_phonenumber']
-            ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:telephonyprovider_list',
-        link_text='Providers',
-        permissions=['netbox_phonebox.view_telephonyprovider'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:telephonyprovider_add',
-                title='Add',
-                icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_phonebox.add_telephonyprovider']
-            ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:pbxserver_list',
-        link_text='PBX Servers',
-        permissions=['netbox_phonebox.view_pbxserver'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:pbxserver_add',
-                title='Add',
-                icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_phonebox.add_pbxserver']
-            ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:extension_list',
-        link_text='Extensions',
-        permissions=['netbox_phonebox.view_extension'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:extension_add',
-                title='Add',
-                icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_phonebox.add_extension']
-            ),
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:make_call',
-                title='Make Call',
-                icon_class='mdi mdi-phone',
-                permissions=['netbox_phonebox.view_extension']
-            ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:siptrunk_list',
-        link_text='SIP Trunks',
-        permissions=['netbox_phonebox.view_siptrunk'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:siptrunk_add',
-                title='Add',
-                icon_class='mdi mdi-plus-thick',
-                permissions=['netbox_phonebox.add_siptrunk']
-            ),
-        )
-    ),
-    PluginMenuItem(
-        link='plugins:netbox_phonebox:calllog_list',
-        link_text='Call Logs',
-        permissions=['netbox_phonebox.view_calllog'],
-        buttons=(
-            PluginMenuButton(
-                link='plugins:netbox_phonebox:call_statistics',
-                title='Statistics',
-                icon_class='mdi mdi-chart-bar',
-                permissions=['netbox_phonebox.view_calllog']
-            ),
-        )
     ),
 )
